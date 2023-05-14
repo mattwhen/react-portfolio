3843c4b929c7f9c98c4ba583c49f2b00b51b3d65
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-05-14 16:52:14 -0500

    This commit adds Resume in the 'Resume' page, and using Flexbox to align properly in the center in a column format

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import myResume from '../../utils/Current-Resume.pdf'
+import myResume_1 from '../../utils/Current-Resume1024_1.jpg';
+import myResume_2 from '../../utils/Current-Resume1024_2.jpg';
 
 export default function Resume() {
   return (
-    <div>
-      <img src={myResume}/>
+    <>
+    <div className='resume-container'>
+      <img src={myResume_1} alt='My personal resume' className='resume-img'/>
+      <img src={myResume_2} alt='My personal resume'className='resume-img'/>
     </div>
+    </>
   );
+
 }
